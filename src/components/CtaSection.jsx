cd8203963fb88be781404686d36431aba4ea0850
sachinlawaniya <sachinlawaniya1234@gmail.com>
2026-08-06 16:28:34 +0530

    Hide slider navigation arrows in mobile view

--- src/components/CtaSection.jsx
+++ src/components/CtaSection.jsx
@@ -23,7 +23,7 @@ export default function CtaSection({ onOpenBrochure }) {
                 <polyline points="7 10 12 15 17 10" />
                 <line x1="12" y1="15" x2="12" y2="3" />
               </svg>
-              <span>Download Brochure &amp; Pricing</span>
+              <span>Download Brochure</span>
             </button>
 
             <button className="er_cta-btn-secondary" onClick={onOpenBrochure}>
